91cc3147a43b86d20a84c3182e398b00028e487b
pvt-16 <prabha0106@gmail.com>
2018-05-15 12:21:25 +0530

    Adding all css, js, loader and config files
    Adding these files after modification. Compare with the original files on https://github.com/aws-samples/aws-lex-web-ui

---
+++ aws-config.js
@@ -0,0 +1,6 @@
+
+const aws_bots_config = 
+'[{"name":"Auro","alias":"AuroTest","description":"Aurobot.","bot-template":"bot-flowers","region":"us-east-1"}]';
+const aws_cognito_identity_pool_id = 'us-east-1:c72f0cbf-c2fc-4cc3-840a-b7f497969c08';
+const aws_cognito_region = 'us-east-1';
+const logger = 'console';
